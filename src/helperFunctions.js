08e54739f3d40c0e7180600e1a3c0ae8d97ef973
JacksonWengler <jacksonwengler@gmail.com>
2026-08-06 14:53:44 -0500

    2 new helper functions. get project name by id & get collaborator user ids for a project

--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -1,4 +1,5 @@
 import {getSingleProject} from "./api/projects.js";
+import useCollaborators from "./hooks/useCollaborators.js";
 
 export function addIfNotEmpty(obj, key, value) {
     if (value?.trim()) {
@@ -9,9 +10,24 @@ export function addIfNotEmpty(obj, key, value) {
 export async function getProjectOwnerId(projectId) {
     try {
         const project = await getSingleProject(projectId)
-        console.log('project repsonse:', project)
         return project?.owner_user_id || null
     } catch (err) {
         return null
     }
+}
+
+export async function getProjectName(projectId) {
+    try {
+        const project = await getSingleProject(projectId)
+        return project?.title || null
+    } catch (err) {
+        return null
+    }
+}
+
+export function getCollabUserIds(projectId) {
+    const {data: collaboratorIds} = useCollaborators(projectId, {
+        select: (res) => res?.collaborator_user_ids ?? res
+    })
+    return collaboratorIds
 }
