1dc8e6f163c0430fdc7c1f26f07be4f87db32668
JacksonWengler <jacksonwengler@gmail.com>
2026-08-05 19:58:54 -0500

    helper function to get owner id from project

--- src/helperFunctions.js
+++ src/helperFunctions.js
@@ -1,5 +1,16 @@
+import {getSingleProject} from "./api/projects.js";
+
 export function addIfNotEmpty(obj, key, value) {
     if (value?.trim()) {
         obj[key] = value
+    }
+}
+
+export async function getProjectOwnerId(projectId) {
+    try {
+        const project = await getSingleProject(projectId)
+        return project?.owner_id || null
+    } catch (err) {
+        return null
     }
 }
