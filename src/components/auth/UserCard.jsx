6002fd28cddab3b14bf62886edb0a42495585212
JacksonWengler <jacksonwengler@gmail.com>
2026-08-10 16:20:12 -0500

    error handling in usercard

--- src/components/auth/UserCard.jsx
+++ src/components/auth/UserCard.jsx
@@ -1,9 +1,15 @@
 import useUser from "../../hooks/useUser.js";
 import styles from "./UserCard.module.css"
 import {Link} from "react-router-dom";
+import LoadingSpinner from "../common/LoadingSpinner.jsx";
+import ErrorPage from "../common/ErrorPage.jsx";
 
 export function UserCard({userId}) {
-    const {user} = useUser(userId)
+    const {user, isLoading, isError,error} = useUser(userId)
+
+    if (isLoading) return <LoadingSpinner/>
+    if (isError) return <ErrorPage message={error.message}/>
+    if (!user) return null
 
     return (
         <Link to={`/user/${userId}`}>
--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -2,9 +2,10 @@ import {useQuery} from "@tanstack/react-query";
 import {getUser} from "../api/auth.js";
 
 export default function useUser(userId) {
-    return useQuery({
+    const query =  useQuery({
         queryKey: ["user", userId],
         queryFn: () => getUser(userId),
         enabled: !!userId
     })
+    return {...query, user: query.data}
 }
