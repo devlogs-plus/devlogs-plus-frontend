3077099e808a5336df3a11eb531e07c0786d708a
JacksonWengler <jacksonwengler@gmail.com>
2026-08-11 16:28:43 -0500

    make user page and own user page display the avatar

--- src/components/auth/OwnUserPage.jsx
+++ src/components/auth/OwnUserPage.jsx
@@ -2,6 +2,7 @@ import LoadingSpinner from "../common/LoadingSpinner.jsx";
 import {Link, Navigate, useNavigate} from "react-router-dom";
 import {useAuth} from "../../context/AuthContext.jsx";
 import {Button} from "../common/Button.jsx";
+import styles from "./UserPage.module.css";
 
 export function OwnUserPage() {
     const {user, isLoading} = useAuth()
@@ -13,8 +14,19 @@ export function OwnUserPage() {
     return (
         <div>
             <h3>{user.display_name}</h3>
+            {user.avatar_url
+                ? (
+                    <img
+                        src={user.avatar_url}
+                        alt="avatar"
+                        className={styles.img}
+                    />
+                )
+                : (
+                    <img src="/default_user_photo.png" alt="deafult avatar" className={styles.img}/>
+                )
+            }
             <p>Email: {user.email}</p>
-            <p>Avatar Url: {user.avatar_url ?? "No avatar set"}</p>
             <p>User Id: {user.id}</p>
             <Link to="/me/edit"><Button>Edit</Button></Link>
         </div>
--- src/components/auth/UserPage.jsx
+++ src/components/auth/UserPage.jsx
@@ -2,6 +2,7 @@ import {useParams} from "react-router-dom";
 import useUser from "../../hooks/useUser.js";
 import LoadingSpinner from "../common/LoadingSpinner.jsx";
 import ErrorPage from "../common/ErrorPage.jsx";
+import styles from "./UserPage.module.css"
 
 export function UserPage() {
     const {userId} = useParams()
@@ -13,8 +14,18 @@ export function UserPage() {
     return (
         <div>
             <h3>{user.display_name}</h3>
-            <p>Email: {user.email}</p>
-            <p>Avatar Url:{user.avatar_url ?? "No avatar set"}</p>
+            {user.avatar_url
+                ? (
+                    <img
+                        src={user.avatar_url}
+                        alt="avatar"
+                        className={styles.img}
+                    />
+                )
+                : (
+                    <img src="/default_user_photo.png" alt="deafult avatar" className={styles.img}/>
+                )
+            }
             <p>User Id: {user.id}</p>
         </div>
     )
