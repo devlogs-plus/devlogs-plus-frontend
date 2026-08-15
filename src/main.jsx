558284a6371428b341e70710a9bd1a14be89433e
JacksonWengler <jacksonwengler@gmail.com>
2026-08-15 14:18:53 -0500

    update project to enable react buddy support

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,8 @@ import {AuthProvider} from './context/AuthContext.jsx'
 import '@fontsource-variable/stack-sans-text';
 import './styles/theme.css'
 import './styles/base.css'
+import {ComponentPreviews, useInitial} from "./dev/index.js";
+import {DevSupport} from "@react-buddy/ide-toolbox";
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -24,7 +26,11 @@ createRoot(document.getElementById('root')).render(
         <QueryClientProvider client={queryClient}>
             <BrowserRouter>
                 <AuthProvider>
-                    <App />
+                    <DevSupport ComponentPreviews={ComponentPreviews}
+                                useInitialHook={useInitial}
+                    >
+                        <App/>
+                    </DevSupport>
                 </AuthProvider>
             </BrowserRouter>
         </QueryClientProvider>
