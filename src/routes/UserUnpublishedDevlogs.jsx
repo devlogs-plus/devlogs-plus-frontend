7635f2eee264e6d46033b0fa6873337abf9a628a
JacksonWengler <jacksonwengler@gmail.com>
2026-08-27 18:26:20 -0500

    make it so that the publish and edit buttons in the unpublished devlog page are grouped togther

--- src/routes/UserUnpublishedDevlogs.jsx
+++ src/routes/UserUnpublishedDevlogs.jsx
@@ -6,22 +6,18 @@ import LoadingSpinner from "../components/common/LoadingSpinner.jsx";
 import usePublishDevlog from "../hooks/usePublishDevlog.js";
 import {useState} from "react";
 import ErrorPage from "../components/common/ErrorPage.jsx";
-import useEditDevlog from "../hooks/useEditDevlog.js";
 import {Link} from "react-router-dom";
 import {Button} from "../components/common/Button.jsx";
+import styles from "./UserUnpublisheDevlogs.module.css"
 
 export function UserUnpublishedDevlogs() {
     const { user, isLoading: authLoading } = useAuth();
-
-    if (authLoading) return <LoadingSpinner/>;
-
-    if (!user) return <UnauthorizedRoute/>
-
     const { devlogs = [], loading, error, refresh } = useUnpublishedDevlogs();
     const publishMutation = usePublishDevlog()
-    const editMutation = useEditDevlog()
     const [publishingId, setPublishingId] = useState(null)
 
+    if (authLoading) return <LoadingSpinner/>;
+    if (!user) return <UnauthorizedRoute/>
     if (loading) return <LoadingSpinner/>
     if (error) return <ErrorPage message={error.message}/>
     if (!devlogs.length) return <div>No unpublished devlogs</div>
@@ -50,8 +46,8 @@ export function UserUnpublishedDevlogs() {
         <>
             <h1>Unpublished Devlogs</h1>
             {devlogs.map(d => (
-                <div key={d.id}>
-                    <DevlogsCard devlog={d} />
+                <div key={d.id} className={styles.devlogCard}>
+                    <DevlogsCard devlog={d} bg={false}/>
                     <div>
                         <Button onClick={() => handlePublish(d)} disabled={publishingId === d.id || publishMutation.isLoading}>
                             {publishingId === d.id ? "Publishing..." : "Publish"}
