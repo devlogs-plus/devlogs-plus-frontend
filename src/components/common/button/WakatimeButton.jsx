60847587a8d50c2d37ec12add7427445cda81673
JacksonWengler <jacksonwengler@gmail.com>
2026-09-12 14:50:10 -0500

    wakatime connect and disconnect button

---
+++ src/components/common/button/WakatimeButton.jsx
@@ -0,0 +1,18 @@
+import {Button} from "../Button.jsx";
+import {connectWaka, disconnectWaka} from "../../../api/auth.js";
+
+export default function WakatimeButton({connect}) {
+    return connect ? (<Button onClick={connectWaka}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="mr-2" viewBox="0 0 1792 1792">
+            <use href="/icons.svg#wakatime-icon"/>
+        </svg>
+        Connect your WakaTime account
+    </Button>) : (
+        <Button onClick={disconnectWaka}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="mr-2" viewBox="0 0 1792 1792">
+                <use href="/icons.svg#wakatime-icon"/>
+            </svg>
+            Disconnect your WakaTime account
+        </Button>
+    )
+}
